7f34c01c3e983a709fa70ba7c96da1bdb843114c
Said Mounaim <saidmounaim00@gmail.com>
2021-05-11 22:03:18 +0000

    Get Single User

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -5,7 +5,21 @@ import User from '../models/User.js';
 //@ROUTE /api/v1/users
 //@METHOD GET
 export const getAll = asyncHandler(async (req, res) => {
-	const users = await User.find({});
+	const users = await User.find({}).populate('posts');
 
 	res.status(201).json({ success: true, count: users.length, data: users });
+});
+
+//@DESC Get Single User
+//@ROUTE /api/v1/users/:id
+//@METHOD GET
+export const getUser = asyncHandler(async (req, res) => {
+	const user = await User.findById(req.params.id).populate('posts');
+
+	if (!user) {
+		res.status(404);
+		throw new Error('User not found');
+	}
+
+	res.status(201).json({ success: true, data: user });
 });
--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { getAll } from '../controllers/UserController.js';
+import { getAll, getUser } from '../controllers/UserController.js';
 
 const router = express.Router();
 
 router.route('/').get(getAll);
+router.route('/:id').get(getUser);
 
 export default router;
